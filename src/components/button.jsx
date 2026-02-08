f1629cd5ff9f895196163c1c4ada62f1fb4a6e1d
Alex Emanuel <alex.emanuel@student.hogent.be>
2026-02-08 21:38:17 +0100

    basis animatie

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,13 +1,37 @@
 import '../index.css';
 import { Link } from 'react-router';
 import { ArrowDownRight } from 'lucide-react';
+import { motion } from "framer-motion";
 
-const Button = ({naam, href}) => {
-    return (
-        <Link to={href} className='link-button'>
-          {naam} <span><ArrowDownRight size={28}/></span>
-        </Link>
-    )
-}
+const MotionLink = motion(Link);
+
+
+const Button = ({ naam, href }) => {
+  return (
+    <MotionLink
+      to={href}
+      className="link-button"
+      whileHover={{
+        scale: 1.05,
+        y: -2
+      }}
+      whileTap={{ scale: 0.97 }}
+      transition={{ type: "spring", stiffness: 300 }}
+    >
+      {naam}
+      <span>
+        <ArrowDownRight size={27} />
+      </span>
+    </MotionLink>
+  );
+};
+
+// const Button = ({naam, href}) => {
+//     return (
+//         <Link to={href} className='link-button'>
+//           {naam} <span><ArrowDownRight size={28}/></span>
+//         </Link>
+//     )
+// }
 
 export default Button;
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,31 +1,72 @@
 import Button from '../button.jsx';
 import './hero.css';
 import { ArrowDownToLine } from 'lucide-react';
+import { motion } from "framer-motion";
+
 
 const Hero = () => {
     return (
         <div className='hero'>
-            <div className='text-content'>
-                <p>Hi, ik ben Alex Emanuel</p>
-                <h1>
-                GRAPHIC DESIGNER <br/>
-                <span className='orangify'>✕</span> DEVELOPER <span className='orangify'>_</span>
-                </h1>
-                <div className='buttons'>
-                <Button naam="Leer mij kennen" href="#"/>
-                <Button naam="Projecten" href="#"/>
-                </div>
+            <div className="text-content">
+                <motion.div initial={{ opacity: 0, y: 30 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                        duration: 0.8,
+                        ease: "easeOut",
+                        delay: 0.2
+                }}>
+                    <p>Hi, ik ben Alex Emanuel</p>
+                    <h1>
+                    GRAPHIC DESIGNER <br/>
+                    <span className='orangify'>✕</span> DEVELOPER <span className='orangify'>_</span>
+                    </h1>
+                    <div className='buttons'>
+                    <Button naam="Leer mij kennen" href="#"/>
+                    <Button naam="Projecten" href="#"/>
+                    </div>
+                </motion.div>
 
                 <div className='socials'>
                     <div>
-                        <a target='blank' href="https://www.linkedin.com/in/alex-emanuel/"><i class="fa-brands fa-linkedin"></i></a>
-                        <a target='blank' href="https://github.com/Alex-Emanuel"><i class="fa-brands fa-github"></i></a>
-                        <a className='download' href="/cv-alex-emanuel.pdf" download><ArrowDownToLine strokeWidth={3} className="social-arrow"/> <span>CV</span></a>
+
+                        <motion.a
+                        href="https://www.linkedin.com/in/alex-emanuel/"
+                        target="_blank" style={{ "padding-left": "2px"}}
+                        whileHover={{ x: 3.5, y: -4, scale: 1.1 }}
+                        transition={{ type: "spring", stiffness: 400 }}
+                        >
+                        <i className="fa-brands fa-linkedin"></i>
+                        </motion.a>
+
+                        <motion.a
+                        href="https://github.com/Alex-Emanuel"
+                        target="_blank"
+                        whileHover={{ x: 3.5, y: -4, scale: 1.1 }}
+                        transition={{ type: "spring", stiffness: 400 }}
+                        >
+                        <i className="fa-brands fa-github"></i>
+                        </motion.a>
+
+                        <motion.a
+                        className="download"
+                        href="/cv-alex-emanuel.pdf"
+                        download
+                        whileHover="hover"
+                        initial="rest"
+                        animate="rest"
+                        >
+                        <motion.span
+                            variants={{
+                            rest: { y: 0, scale: 1 },
+                            hover: { y: -5, scale: 1.1 }
+                            }}
+                            transition={{ type: "spring", stiffness: 400, duration: 0.25 }}
+                        >
+                            <ArrowDownToLine strokeWidth={3} className="social-arrow" />
+                        </motion.span>
+                        <span>CV</span>
+                        </motion.a>
                     </div>
-                    {/* <div>
-                        <p>@alex-emanuel</p>
-                        <p>@Alex-Emanuel</p>
-                    </div> */}
                 </div>
             </div>
             <img src='/me.png'></img>
