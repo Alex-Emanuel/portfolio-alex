1aae3d68daef0a26fdf2a2b5aaeb885462979ee0
Alex Emanuel <alex.emanuel@student.hogent.be>
2026-02-08 22:58:53 +0100

    a ipv link voor ankerelement koppeling

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,37 +1,20 @@
 import '../index.css';
-import { Link } from 'react-router';
 import { ArrowDownRight } from 'lucide-react';
 import { motion } from "framer-motion";
 
-const MotionLink = motion(Link);
-
-
 const Button = ({ naam, href }) => {
   return (
-    <MotionLink
-      to={href}
+    <motion.a
+      href={href}
       className="link-button"
-      whileHover={{
-        scale: 1.05,
-        y: -2
-      }}
+      whileHover={{ scale: 1.05, y: -2 }}
       whileTap={{ scale: 0.97 }}
       transition={{ type: "spring", stiffness: 300 }}
     >
       {naam}
-      <span>
-        <ArrowDownRight size={27} />
-      </span>
-    </MotionLink>
+      <span><ArrowDownRight size={27} /></span>
+    </motion.a>
   );
 };
-
-// const Button = ({naam, href}) => {
-//     return (
-//         <Link to={href} className='link-button'>
-//           {naam} <span><ArrowDownRight size={28}/></span>
-//         </Link>
-//     )
-// }
 
 export default Button;
