1a394727d703cca9d567bc73b7678c9d70dc77b9
Alex Emanuel <alex.emanuel@student.hogent.be>
2026-02-10 18:48:32 +0100

    footer afgewerkt

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,16 @@
 import Hero from '../components/hero/hero.jsx';
 import Me from '../components/body/me.jsx';
 import { motion } from "framer-motion";
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import Ervaring from '../components/body/ervaring.jsx';
 import Vaardigheden from '../components/body/vaardigheden.jsx';
 import Projecten from '../components/body/projecten.jsx';
 import Contact from '../components/body/contact.jsx';
+import { useGSAP } from '@gsap/react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const pageVariants = {
     initial: { opacity: 0 },
@@ -14,6 +19,30 @@ const pageVariants = {
 };
 
 const Home = () => {
+  const containerRef = useRef(null);
+
+  useGSAP(() => {
+  if (!containerRef.current) return;
+
+  gsap.fromTo(
+    containerRef.current,
+    { opacity: 0, y: 80 },
+    {
+      opacity: 1,
+      y: 0,
+      ease: "none",
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "top 115%",
+        end: "top 90%",
+        scrub: true,
+        scroller: ".website-content",
+      },
+    }
+  );
+}, []);
+
+
   useEffect(() => {
       document.title = "Alex Emanuel | Design & Dev";
     }, []);
@@ -32,7 +61,18 @@ const Home = () => {
       <Vaardigheden />
       <Projecten />
       <Contact />
-      <p className='footer'>space</p>
+      <footer ref={containerRef}>
+        <p>©2026 Alex Emanuel - Alle rechten voorbehouden</p>
+        <div>
+          <a href="tel:+32484022278">+32 484 02 22 78</a>
+          <a href="https://www.linkedin.com/in/alex-emanuel/" target="_blank">
+              <i className="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/Alex-Emanuel" target="_blank">
+              <i className="fa-brands fa-github"></i>
+          </a>
+        </div>
+      </footer>
     </motion.main>
   )
 }
